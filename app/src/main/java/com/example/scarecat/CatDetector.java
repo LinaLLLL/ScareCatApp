d25a538a0654d5d2920a88af627b025a8e61f226
Lina <linachugainova0@gmail.com>
2025-06-25 09:25:17 +0500

    Создание класса CatDetector, для распознавания кошек

--- app/src/main/java/com/example/scarecat/CatDetector.java
+++ app/src/main/java/com/example/scarecat/CatDetector.java
@@ -1,17 +1,67 @@
 package com.example.scarecat;
 
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.objects.DetectedObject;
-import com.google.mlkit.vision.objects.ObjectDetection;
-import com.google.mlkit.vision.objects.ObjectDetector;
-import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions;
-public class CatDetector {
-    private final ObjectDetector detector;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.util.Log;
 
-    public CatDetector(){
-        ObjectDetectorOptions options = new ObjectDetectorOptions().Builder()
-                .setDetectorMode(ObjectDetectorOptions.STREAM_MODE)
-                .enableClassification() //включаем классификацию
-                .buid();
+import androidx.annotation.NonNull;
+
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+
+import java.util.List;
+
+public class CatDetector {
+    public interface DetectionCallback { //интерфейс обратного вызова (Callback)
+        void onCatDetected(float confidence);
+        void onNoCatDetected();
+        void onError(Exception e);
     }
+
+    private final ImageLabeler labeler; //распознавание
+    private final MediaPlayer mediaPlayer; //воспроизведение звука
+    //конструктор
+    public CatDetector(Context context) {
+        labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+        mediaPlayer = MediaPlayer.create(context, R.raw.cat_sound);
+    }
+
+    public void detectCats(@NonNull InputImage image, DetectionCallback callback) {
+        labeler.process(image)  //запуск ассинхронного процесса распознавания
+                .addOnSuccessListener(labels -> { //получаем список меток
+                    boolean catFound = false;
+                    for (ImageLabel label : labels) {  //перебираем список и ищем с текстом "cat"
+                        if ("Cat".equalsIgnoreCase(label.getText()) && label.getConfidence() > 0.5) { //проверяем уверенность больше 50%
+                            catFound = true;
+
+                            if (!mediaPlayer.isPlaying()) { //если кот найден, запускаем музыку
+                                mediaPlayer.start();
+                            }
+
+                            callback.onCatDetected(label.getConfidence());
+                            break;
+                        }
+                    }
+                    if (!catFound) { // кот не найден
+                        callback.onNoCatDetected();
+                        if (mediaPlayer.isPlaying()) { //если музыка играла - выключаем
+                            mediaPlayer.pause();
+                            mediaPlayer.seekTo(0);
+                        }
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("CatDetector", "Ошибка детекции", e);
+                    callback.onError(e);
+                });
+    }
+    //очистка ресурсов
+    public void release() {
+        labeler.close();
+        mediaPlayer.release();
+    }
+
 }
